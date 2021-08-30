a8c94f4d11e9a90fe0579a8ef48d3097b80953ff
Biran Syangbo <biransyangbo@gmail.com>
2021-08-30 08:47:14 +0545

    Initial Changes

---
+++ src/main/java/org/clusus/bloomberg/BloombergCustomerRecordApplication.java
@@ -0,0 +1,13 @@
+package org.clusus.bloomberg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloombergCustomerRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloombergCustomerRecordApplication.class, args);
+	}
+
+}
